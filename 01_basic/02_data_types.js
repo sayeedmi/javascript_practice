4f9c0ebff477a07e7a4b32d62c5315361658b7c8
Sayeed Mohammed Ibrahim <86453505+sayeedmi@users.noreply.github.com>
2024-05-29 18:05:01 +0000

    update data_types.js file

--- 01_basic/02_data_types.js
+++ 01_basic/02_data_types.js
@@ -2,14 +2,18 @@
 
 // ===> These are Premitive Data Types:
 
-let name = 'sayeed'; // String Data Types 
-let age = 23; // Number Data Types
-let married = false; // Boolean Data Types
-let standaloneValue = null; // 'null' is an standalone value
-let noValue; // 'undifined' is a value 
+let name = 'sayeed'; // 'String' Data Type
+let age = 23; // 'Number' Data Type
+let married = false; // 'Boolean' Data Type
+let standaloneValue = null; // 'null' is an standalone 'Value' and an 'Object' 
+let noValue; // 'undifined' is a value and an 'Undefined' Data Type
 
 //----------------------------------------------------------------------
 
 // ==> Objects:
 
 console.log(typeof name);
+console.log(typeof(age));
+console.log(typeof(married));
+console.log(typeof(null));
+console.log(typeof undefined);
