32d6a86ab9e48a294740e758f49fa6d4cceb5d40
Sayeed Mohammed Ibrahim <86453505+sayeedmi@users.noreply.github.com>
2024-05-29 17:59:08 +0000

    Add data_types.js file

---
+++ 01_basic/02_data_types.js
@@ -0,0 +1,15 @@
+'use strict'; // Treat all JS code as newer version
+
+// ===> These are Premitive Data Types:
+
+let name = 'sayeed'; // String Data Types 
+let age = 23; // Number Data Types
+let married = false; // Boolean Data Types
+let standaloneValue = null; // 'null' is an standalone value
+let noValue; // 'undifined' is a value 
+
+//----------------------------------------------------------------------
+
+// ==> Objects:
+
+console.log(typeof name);
