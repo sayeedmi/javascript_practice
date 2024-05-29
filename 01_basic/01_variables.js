f05e0bb5b7c89bbeca53e8b87413375b40dbf6b8
Sayeed Mohammed Ibrahim <86453505+sayeedmi@users.noreply.github.com>
2024-05-29 17:46:37 +0000

    Update the whole repository.

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,8 @@
+const accountEmail = "sh@gag.com";
+let accountPassword = 12423542;
+var accountCity = "chittagong";
+
+accountPassword = 23425;
+accountCity = 'dakha';
+
+console.table([accountEmail, accountCity, accountPassword]);
