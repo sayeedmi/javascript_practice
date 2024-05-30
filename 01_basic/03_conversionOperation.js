9f70b76e6b8b1ae2ff8e6b3b31ce80c81508abcb
Sayeed Mohammed Ibrahim <86453505+sayeedmi@users.noreply.github.com>
2024-05-30 10:28:17 +0000

    update file

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,4 +1,30 @@
-let score = 33;
+// let score = '33qr';
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // Output: Number X
+// console.log(valueInNumber); // Output: NaN == Not a Number --> JavaScript's Fault
+
+
+//----------------------------------------------------------------------
+
+// let phoneNumber = undefined; // Output: NaN
+// phoneNumber = null; // Output: 0 (Zero)
+// phoneNumber = 'sayeed' // Output: NaN
+// phoneNumber = NaN; // Output: NaN
+// phoneNumber = true; // Output: 1
+
+// let valueInNumber = Number(phoneNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//----------------------------------------------------------------------
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
