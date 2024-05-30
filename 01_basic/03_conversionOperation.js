a00e0e0363ea9bc017a75e08759b1367b68b9849
Sayeed Mohammed Ibrahim <86453505+sayeedmi@users.noreply.github.com>
2024-05-30 10:43:54 +0000

    finish 03_conversionOperation file work

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,30 +1,41 @@
-// let score = '33qr';
+let score = '33qr';
 
-// console.log(typeof score);
-// console.log(typeof(score));
+console.log(typeof score);
+console.log(typeof(score));
 
-// let valueInNumber = Number(score);
-// console.log(typeof valueInNumber); // Output: Number X
-// console.log(valueInNumber); // Output: NaN == Not a Number --> JavaScript's Fault
-
-
-//----------------------------------------------------------------------
-
-// let phoneNumber = undefined; // Output: NaN
-// phoneNumber = null; // Output: 0 (Zero)
-// phoneNumber = 'sayeed' // Output: NaN
-// phoneNumber = NaN; // Output: NaN
-// phoneNumber = true; // Output: 1
-
-// let valueInNumber = Number(phoneNumber);
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+let valueIntoNumber = Number(score);
+console.log(typeof valueIntoNumber); // Output: Number X
+console.log(valueIntoNumber); // Output: NaN == Not a Number --> JavaScript's Fault
 
 
-//----------------------------------------------------------------------
+//----------------------{ Converted into Number }-------------------------------
 
-let isLoggedIn = 1;
-let booleanIsLoggedIn = Boolean(isLoggedIn);
+let phoneNumber = undefined; // Output: NaN
+phoneNumber = null; // Output: 0 (Zero)
+phoneNumber = 'sayeed' // Output: NaN
+phoneNumber = NaN; // Output: NaN
+phoneNumber = true; // Output: 1
 
-console.log(typeof booleanIsLoggedIn);
+let valueInNumber = Number(phoneNumber);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//----------------------{ Converted into Boolean }--------------------------
+
+let isLoggedIn = "0524"; // Output: True
+isLoggedIn = ""; // Output: False -- Because of the "Empty String"
+isLoggedIn = 0; // Output: False
+isLoggedIn = 1;  // Output: True
+let booleanIsLoggedIn = Boolean(isLoggedIn); 
+
+console.log("Type = " + typeof booleanIsLoggedIn);
 console.log(booleanIsLoggedIn);
+
+
+//----------------------{ Converted into String }-------------------------------
+
+let aNumber = 9;
+aNumber = String(aNumber); 
+console.log(typeof aNumber);
+console.log(aNumber);
